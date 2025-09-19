16672c8d9434bb7de02b729379e4f4af4b374156
Tommi Koskinen <156694715+pyjamaB@users.noreply.github.com>
2025-09-19 23:41:49 +0300

    Change how login works

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import sqlite3
 from flask import Flask
 from flask import abort, redirect, render_template, request, session
-from werkzeug.security import check_password_hash, generate_password_hash
 import config
 import db
 import reviews
@@ -120,14 +119,10 @@ def create():
     password2 = request.form["password2"]
     if password1 != password2:
         return "VIRHE: salasanat eivät ole samat"
-    password_hash = generate_password_hash(password1, method='pbkdf2')
-
     try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
+        users.create_user(username, password1)
     except sqlite3.IntegrityError:
         return "VIRHE: tunnus on jo varattu"
-
     return "Tunnus luotu"
 
 @app.route("/login", methods=["GET", "POST"])
@@ -138,13 +133,9 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-    
-        sql = "SELECT id, password_hash FROM users WHERE username = ?"
-        result = db.query(sql, [username])[0]
-        user_id = result["id"]
-        password_hash = result["password_hash"]
 
-        if check_password_hash(password_hash, password):
+        user_id = users.check_login(username, password)
+        if user_id:
             session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
--- users.py
+++ users.py
@@ -1,3 +1,4 @@
+from werkzeug.security import check_password_hash, generate_password_hash
 import db
 
 def get_user(user_id):
@@ -8,3 +9,20 @@ def get_user(user_id):
 def get_reviews(user_id):
     sql = "SELECT id, title FROM items WHERE user_id = ? ORDER BY id DESC"
     return db.query(sql, [user_id])
+
+def create_user(username, password1):
+    password_hash = generate_password_hash(password1, method='pbkdf2')
+    sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+    db.execute(sql, [username, password_hash])
+
+def check_login(username, password):
+    sql = "SELECT id, password_hash FROM users WHERE username = ?"
+    result = db.query(sql, [username])
+    if not result:
+        return None
+    user_id = result[0]["id"]
+    password_hash = result[0]["password_hash"]
+    if check_password_hash(password_hash, password):
+        return user_id
+    else:
+        return None
