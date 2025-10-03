6d786fc8d5f5e40fc08a42827f0f906951807db1
Tommi Koskinen <156694715+pyjamaB@users.noreply.github.com>
2025-10-03 11:41:06 +0300

    Block csrf vulnerability

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import secrets
 import sqlite3
 from flask import Flask
 from flask import abort, flash, make_response, redirect, render_template, request, session
@@ -11,6 +12,12 @@ app.secret_key = config.secret_key
 
 def require_login():
     if "user_id" not in session:
+        abort(403)
+
+def check_csrf():
+    if "csrf_token" not in request.form:
+        abort(403)
+    if request.form["csrf_token"] != session["csrf_token"]:
         abort(403)
 
 @app.route("/")
@@ -68,6 +75,7 @@ def edit_images(item_id):
 @app.route("/add_image", methods=["POST"])
 def add_image():
     require_login()
+    check_csrf()
 
     item_id = request.form["item_id"]
     item = reviews.get_review(item_id)
@@ -92,6 +100,7 @@ def add_image():
 @app.route("/remove_images", methods=["POST"])
 def remove_images():
     require_login()
+    check_csrf()
 
     item_id = request.form["item_id"]
     item = reviews.get_review(item_id)
@@ -108,6 +117,7 @@ def remove_images():
 @app.route("/create_message", methods=["POST"])
 def create_message():
     require_login()
+    check_csrf()
     
     content = request.form["content"]
     if not content or len(content) > 5000:
@@ -131,6 +141,7 @@ def new_item():
 @app.route("/create_item", methods=["POST"])
 def create_item():
     require_login()
+    check_csrf()
     title = request.form["title"]
     if not title or  len(title) > 150:
         abort(403)
@@ -176,6 +187,7 @@ def edit_item(item_id):
 @app.route("/update_item", methods=["POST"])
 def update_item():
     require_login()
+    check_csrf()
     item_id = request.form["item_id"]
     item = reviews.get_review(item_id)
     if not item:
@@ -218,6 +230,7 @@ def delete_item(item_id):
         return render_template("delete_item.html", item=item)
 
     if request.method == "POST":
+        check_csrf()
         if "delete" in request.form:
             reviews.delete_review(item_id)
             return redirect("/")
@@ -257,6 +270,7 @@ def login():
         if user_id:
             session["user_id"] = user_id
             session["username"] = username
+            session["csrf_token"] = secrets.token_hex(16)
             return redirect("/")
         else:
             flash("VIRHE: Väärä tunnus tai salasana")
